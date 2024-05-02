e5bc03afe41d832b7b3f9c30c7861ba2d597a645
Daks(jongsu Baek) <51820806+daks4012@users.noreply.github.com>
2024-05-02 13:29:25 +0900

    update

--- REACT/shop/src/pages/Product.js
+++ REACT/shop/src/pages/Product.js
@@ -1,35 +1,41 @@
+// Product.js
+
 import perfumeData from '../assets/data/perfume';
 import Card from '../components/Card'
-import { Container, Row } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 
 function Product() {
     let [perfume, setPerfume] = useState(perfumeData);
     useEffect(() => {
-        axios.get('https://daks4012.github.io/REACT/shop/dummy/newPerfume.js').then((result) => {
-            let perfumeSum = [...perfume, ...result.data];
-            setPerfume(perfumeSum);
-        })
-    }, [])
+        axios.get('https://daks4012.github.io/REACT/shop/dummy/newPerfume.js')
+            .then((result) => {
+                setPerfume([...perfumeData, ...result.data]);
+            })
+            .catch((error) => {
+                console.error('Error fetching perfume data:', error);
+            });
+    }, []);
     return (
         <>
             <div className='porductTitle'>
                 <h4>Product</h4>
             </div>
             <Container>
-                {perfume.map(function (a, i) {
-                    return (
-                        i % 3 === 0 ?
-                            <Row>
-                                <Card perfume={perfume} i={i} />
-                                <Card perfume={perfume} i={i + 1} />
-                                <Card perfume={perfume} i={i + 2} />
-                            </Row>
-                            :
-                            null
-                    )
-                })}
+                <Row>
+                    {perfume.map((product, index) => (
+                        <Col key={index} sm={4}>
+                            <Card
+                                title={product.title}
+                                content={product.content}
+                                price={product.price}
+                                index={index}
+                                imageUrl={`${process.env.PUBLIC_URL}/assets/img/perfume${index}.png`}
+                            />
+                        </Col>
+                    ))}
+                </Row>
             </Container>
         </>
     )
